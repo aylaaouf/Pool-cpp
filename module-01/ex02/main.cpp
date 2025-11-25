a698bbd2766c940a6b12b831a94932e05b368aeb
aylaaouf <ayoub.laaoufi@hotmail.com>
2025-11-25 02:47:33 +0100

    pointers

---
+++ module-01/ex02/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string brain = "HI THIS IS BRAIN";
+    std::string *stringPTR = &brain;
+    std::string &stringREF = brain;
+
+    std::cout << &brain << "\n";
+    std::cout << stringPTR << "\n";
+    std::cout << &stringREF << "\n";
+
+    std::cout << brain << "\n";
+    std::cout << *stringPTR << "\n";
+    std::cout << stringREF << "\n";
+}
