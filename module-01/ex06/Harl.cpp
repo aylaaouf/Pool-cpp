49a482e182cfcaf1b570689ec51636ce877eb9d3
aylaaouf <ayoub.laaoufi@hotmail.com>
2025-11-27 02:14:46 +0100

    switch cases

---
+++ module-01/ex06/Harl.cpp
@@ -0,0 +1,41 @@
+#include "Harl.hpp"
+
+void Harl::debug() {
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\n";
+}
+
+void Harl::info() {
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t putenough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
+}
+
+void Harl::warning() {
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month.\n";
+}
+
+void Harl::error() {
+    std::cout << "This is unacceptable! I want to speak to the manager now.\n";
+}
+
+void Harl::complain( std::string level ) {
+    std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    int index = -1;
+    for (int i = 0; i < 4; i++) {
+        if (level == levels[i]) {
+            index = i;
+            break ;
+        }
+    }
+    switch ((index)) {
+        case 0:
+            debug();
+        case 1:
+            info();
+        case 2:
+            warning();
+        case 3:
+            error();
+            break ;
+        default:
+            std::cout << "[ Probably complaining about insignificant problems ]\n";
+    }
+}
